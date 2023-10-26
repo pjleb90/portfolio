7176e66b64887832704ddc0eac35dc55211103b7
pjleb90 <pjleb90@gmail.com>
2023-10-26 07:00:36 -0400

    landing page formatting

--- components/featured.tsx
+++ components/featured.tsx
@@ -7,7 +7,10 @@ const Featured = () => {
             <div className="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-6 lg:grid-cols-12">
                 <div className="mr-auto place-self-center lg:col-span-7">
                     <h1 className="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl">Let&#39;s make your dream website a reality</h1>
-                    <p className="max-w-2xl mb-6 font-light lg:mb-8 md:text-lg lg:text-xl text-gray-800 dark:text-white">From checkout to global sales tax compliance, companies around the world use Flowbite to simplify their payment stack.</p>
+                    <p className="max-w-2xl mb-6 font-light lg:mb-8 md:text-lg lg:text-xl text-gray-800 dark:text-white">
+                        From responsive designs to search engine optimization, if you need a new website to help you stand out or
+                        if you&#39;re not happy with your existing website, I&#39;d love to work with you.
+                    </p>
                     <a href="/contact" className="bg-gradient-to-r from-cyan-500 to-teal-400 text-gray-800
                                 dark:text-grey-800 focus:ring-4 focus:outline-none rounded-lg px-4 py-2
                                 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700">
@@ -23,7 +26,7 @@ const Featured = () => {
                         src="https://img.freepik.com/free-vector/laptop-with-program-code-isometric-icon-software-development-programming-applications-dark-neon_39422-971.jpg?size=626&ext=jpg&ga=GA1.1.867424154.1698192000&semt=sph"
                         // src="https://img.freepik.com/premium-vector/isometric-developing-programming-coding-technologies-young-programmer-coding-new-project-using-computer-man-sitting-big-laptop-working_589019-2494.jpg"
                         width={500} height={500}
-                        alt='an image of a portfolio project'
+                        alt='a picture of a laptop with code'
                         style={{ borderRadius: '60px' }}
                     />
                 </div>
--- components/header.tsx
+++ components/header.tsx
@@ -5,7 +5,7 @@ const Header = () => {
         <section>
           <div className='text-center p-10'>
             <h2 className='text-4xl py-2 text-center text-teal-600 font-medium md:text-6xl mx-auto'>PJ LeBlanc</h2>
-            <h3 className='text-2xl py-2 md:text-3xl'>Web Developer</h3>
+            <h3 className='text-2xl py-2 md:text-3xl'>Developer and Designer</h3>
             <p className='text-md py-5 text-gray-800 md:text-xl max-w-lg mx-auto dark:text-white'>
               A freelancer providing services for web design and development. Looking forward to hearing from you!
             </p>
