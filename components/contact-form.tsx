c7c4b763cfa8d95d8b256d7ca56abd54827b4641
pjleb90 <pjleb90@gmail.com>
2023-10-27 07:00:34 -0400

    UX/UI improvements

--- components/contact-form.tsx
+++ components/contact-form.tsx
@@ -123,7 +123,7 @@ export const ContactForm = () => {
                     </div>
                     <Button type="submit" className="bg-gradient-to-r from-cyan-500 to-teal-400 text-gray-800
                         dark:text-grey-800 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg px-4 py-2
-                        text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                        text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 w-full md:w-auto"
                     >
                         Send message
                     </Button>
--- components/featured.tsx
+++ components/featured.tsx
@@ -6,7 +6,7 @@ const Featured = () => {
         <section className="bg-white dark:bg-gray-800">
             <div className="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-6 lg:grid-cols-12">
                 <div className="mr-auto place-self-center lg:col-span-7">
-                    <h1 className="max-w-2xl mb-4 text-4xl font-bold tracking-tight leading-none md:text-5xl xl:text-5xl">Let&#39;s make your dream website a reality</h1>
+                    <h1 className="max-w-xl mb-4 text-4xl font-bold tracking-tight leading-none text-center md:text-5xl xl:text-5xl">Let&#39;s make your dream website a reality</h1>
                     <p className="max-w-2xl mb-6 font-light lg:mb-8 md:text-lg lg:text-xl text-gray-800 dark:text-white">
                         From responsive designs to search engine optimization, if you need a new website to help you stand out or
                         if you&#39;re not happy with your existing one, I&#39;d love to work with you.
