eeecca31773fc1a2dcbdd9c7b9e0bd9fff3e0e1d
pjleb90 <pjleb90@gmail.com>
2023-11-19 16:39:33 -0500

    small UI changes

--- components/theme-toggle.tsx
+++ components/theme-toggle.tsx
@@ -20,7 +20,7 @@ export function ThemeToggle() {
   return (
     <Button variant="naked" size="icon" onClick={toggleTheme}>
       {isClient && theme === "light" && (
-        <FaMoon className="w-5 h-5 text-gray-600 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0 hover:text-teal-500 animate-bounce" />
+        <FaMoon className="w-5 h-5 text-gray-600 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0 hover:text-teal-500" />
       )}
       {isClient && theme !== "light" && (
         <FaSun className="w-5 h-5 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100 hover:text-white animate-flicker" />
