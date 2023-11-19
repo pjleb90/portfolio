ed3e1143008d8d8cf6847c1568ff5508d1df9a53
pjleb90 <pjleb90@gmail.com>
2023-11-19 14:38:37 -0500

    added animations to the theme-toggle

--- tailwind.config.js
+++ tailwind.config.js
@@ -65,10 +65,17 @@ module.exports = {
           from: { height: "var(--radix-accordion-content-height)" },
           to: { height: 0 },
         },
+        flicker: {
+          '0%': { opacity: '1' },
+          '25%': { opacity: '0.5' },
+          '50%': { opacity: '1' },
+          '100%': { opacity: '0.5' }
+        },
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
+        flicker: 'flicker .8s ease-out infinite alternate',
       },
     },
   },
