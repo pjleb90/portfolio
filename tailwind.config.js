2f9b49fa548767d42e8db2746c576532e3912c3f
pjleb90 <pjleb90@gmail.com>
2023-11-21 19:21:56 -0500

    added a infinite horizontal scroll to the Project page

--- tailwind.config.js
+++ tailwind.config.js
@@ -71,11 +71,17 @@ module.exports = {
           '50%': { opacity: '1' },
           '100%': { opacity: '0.5' }
         },
+        infiniteScroll: {
+          from: { transform: 'translateX(0)' },
+          to: { transform: 'translateX(-100%)' },
+        }
+
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
         flicker: 'flicker .8s ease-out infinite alternate',
+        infiniteScroll: 'infiniteScroll 50s linear infinite',
       },
     },
   },
